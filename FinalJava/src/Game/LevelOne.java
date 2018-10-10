dba0e6bd5819f85ef76a3ec548bd4dd9ae41b79a
Gio Jacobsen <superpieman157@yahoo.com>
2018-10-10 09:48:12 -0500

    Finished level one
    Did all of level ones code

--- FinalJava/src/Game/LevelOne.java
+++ FinalJava/src/Game/LevelOne.java
@@ -2,12 +2,75 @@ package Game;
 
 import javax.swing.JOptionPane;
 
+import java.util.Random;
+
 public class LevelOne {
 
 	public static void LevelOne() 
 	{
-		Chip.Chips();
-		JOptionPane.showMessageDialog(null, chip);
+		String chipChoice;
+		
+		Random rand = new Random();
+		
+     	  int E = rand.nextInt(2) + 1;
+        
+		chipChoice = JOptionPane.showInputDialog(null,
+		        "What Is Your Favorite chip?",
+		        "A Questioning Box",
+		        JOptionPane.QUESTION_MESSAGE);
+		
+        JOptionPane.showMessageDialog(null, "Hello and welcome to... ",
+       		 "A Welcoming box", 
+       		 JOptionPane.PLAIN_MESSAGE);
+        
+        JOptionPane.showMessageDialog(null, "The Time Killer",
+       		 "An Evil Box", 
+       		 JOptionPane.ERROR_MESSAGE);
+        
+        int KeepGoing = JOptionPane.showConfirmDialog(null, 
+       		 "Have you ever been so betrayed you'd do anything to get them back?", 
+       		 "A Question box", JOptionPane.YES_NO_OPTION);
+        if(KeepGoing == JOptionPane.NO_OPTION)
+        {
+       	  if (E == 1)
+       	  {
+       		  JOptionPane.showMessageDialog(null, "Then I want you to play this game to "
+       		  		+ "\nshow you how sweet revenge can be. "
+       		  		+ "\nThere is a catch though if you get caught this experiment "
+       		  		+ "\nwill end and you will go to "
+       		  		+ "\njail for the rest of your life.",
+       		  		"An Evil Box",
+       		  		JOptionPane.ERROR_MESSAGE);
+       	  }
+       	  else
+       	  {
+       		  System.exit(0);
+       	  }
+        }
+        else
+        {
+        	if (E == 1)
+        	{
+        		JOptionPane.showMessageDialog(null, "Then I want you to play this game to "
+           		  		+ "\nshow you how sweet revenge can be. "
+           		  		+ "\nThere is a catch though if you get caught this experiment "
+           		  		+ "\nwill end and you will go to "
+           		  		+ "\njail for the rest of your life. ",
+           		  		"An Evil Box",
+           		  		JOptionPane.ERROR_MESSAGE);
+        	}
+        	else
+        	{
+        		JOptionPane.showMessageDialog(null, " You have chosen to continue. "
+        				+ "Lets begin the game.",
+        				"A Continuation box",
+        				JOptionPane.PLAIN_MESSAGE);
+        		LevelTwo.LevelTwo(chipChoice);
+        	}
+        }
+		
 	}
+
+	
 
 }
--- FinalJava/src/Game/LevelTwo.java
+++ FinalJava/src/Game/LevelTwo.java
@@ -1,10 +1,25 @@
 package Game;
 
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
 public class LevelTwo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void LevelTwo(String chip) 
+	{
+		
+		UIManager.put(JOptionPane.YES_BUTTON_TEXT, "Run in and murder them all!");
+		UIManager.put(JOptionPane.NO_OPTION, "Walk in calmly and hand out the snacks and sit down.");
+		JOptionPane.showConfirmDialog(null, "You are driving back from gas station to get snacks for "
+				+ "\n you and some of your close friends and as you walk in you hear your friends "
+				+ "\n talking. You hear Dianne talking about you saying " + "We should take the food and "
+						+ "\n just go home shes not good enough to be our friend and we all know that"
+				+ "\n Everyone bursts into laughter and Kat says " + "Shes not even a good friend she "
+						+ "\n just does everything we want because shes so desperate to be our friend and "
+						+ "\n feel wanted. Haha she'd probably die without us. Maybe she should find "
+						+ "\n people who actually want to be her friend" + "What do you do?",
+						"An Option Box",
+						JOptionPane.YES_NO_CANCEL_OPTION);
 	}
 
 }
