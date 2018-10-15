1072ed72d049483658ff2add441fd9da4822e40e
Faith Toppert <faithtoppert@gmail.com>
2018-10-15 09:53:40 -0500

    Main

--- FinalJava/src/Game/Main.java
+++ FinalJava/src/Game/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) 
 	{
         LevelOne.LevelOne();
+        LevelTwo.LevelTwo();
         LevelThree.LevelThree();
         LevelFour.LevelFour();
         LevelFive.LevelFive();
