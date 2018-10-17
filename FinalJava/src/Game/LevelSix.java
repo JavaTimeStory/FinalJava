d0046005b903ec8929cc8f69692b763a1dbadba0
Faith Toppert <faithtoppert@gmail.com>
2018-10-17 08:56:02 -0500

    levels

--- FinalJava/src/Game/LevelSix.java
+++ FinalJava/src/Game/LevelSix.java
@@ -11,12 +11,12 @@ public class LevelSix {
 						+ "\n They aren't worth it. I want to go back to the future \r"
 						+ "\n and be just as happy without them not as a muderer. "};
         int Choice = JOptionPane.showOptionDialog(null, "Your machines are made and ready to help you murder "
-        		+ "\n these awful friends. You step into the machine scared out of your mind "
-        		+ "\n and set the time to right before everyone gets to the sleepover when "
-        		+ "\n just Dianne and Kat were there. You blast off and arrive at your house. "
+        		+ "\n these awful coworkers. You step into the machine scared out of your mind "
+        		+ "\n and set the time to right before everyone gets to work when it is "
+        		+ "\n just Dianne and Kat there. You blast off and arrive at your work. "
         		+ "\n You see yourself  having a deep conversation with Anna about how happy "
-        		+ "\n you are to be her friend and how you and how you will be friends with "
-        		+ "\n her through anything. You'd be friends with her forever...."
+        		+ "\n you are to be her coworker and even her friend and how you and how you want to be friends with "
+        		+ "\n her through anything. You'd be there for her coworker or not...."
         		+ "\n Jeez you really cared about them. Does it really matter what they think "
         		+ "\n about you? Do you know you're a good person and amazing friend worth "
         		+ "\n being there for? Do you still even want to murder them?",
